ccc0c3f865df3a47881731a62ff2fe47d3819fd2
Spica TSUKIMI <haochengvanadis@gmail.com>
2022-10-11 18:44:40 -0400

    Add Baseline model

--- a2/A2_Gradio/A2_Gradio.py
+++ a2/A2_Gradio/A2_Gradio.py
@@ -4,42 +4,49 @@ import torchtext
 import gradio as gr
 try:
     from Classifier import Conv2dWordClassifier
+    from Baseline import Baseline
 except Exception as e:
     print(e)
     from A2_Gradio.Classifier import Conv2dWordClassifier
+    from A2_Gradio.Baseline import Baseline
 
 
 glove = torchtext.vocab.GloVe(name="6B",dim=100) # embedding size = 100
 
-path1 = "A2_Gradio/models/model_cnn_10112022_175513.pt"
-args1 = DotMap({'k1': 2, 'k2': 4, 'n1': 4, 'n2': 16, 'freeze_embedding': False, 'bias': False})
+path1 = "A2_Gradio/models/model_baseline_lr_0.001_bs_16_epochs_50_10112022_154743.pt"
+# args1 = DotMap({'k1': 2, 'k2': 4, 'n1': 4, 'n2': 16, 'freeze_embedding': False, 'bias': False})
 path2 = "A2_Gradio/models/model_cnn_10112022_174856.pt"
 args2 = DotMap({'k1': 2, 'k2': 5, 'n1': 16, 'n2': 32, 'freeze_embedding': False, 'bias': False})
 
 def predict(sentence, model):
-    if model == "model 2":
-        m = Conv2dWordClassifier(glove, args2)
-        m.load_state_dict(torch.load(path2))
-    else:
-        m = Conv2dWordClassifier(glove, args1)
-        m.load_state_dict(torch.load(path1))
-    m.eval()
     tokens = sentence.split()
     token_ints = [glove.stoi.get(tok, len(glove.stoi)-1) for tok in tokens]
     token_tensor = torch.LongTensor(token_ints).view(-1,1)
-    _, logit = m(token_tensor)
-    pred = torch.round(logit).long()
-    return "Subjective" if pred else "Objective"
+    if model == "CNN":
+        m = Conv2dWordClassifier(glove, args2)
+        m.load_state_dict(torch.load(path2))
+        m.eval()
+        try:
+            _, logit = m(token_tensor)
+            pred = torch.round(logit).long()
+            return "Subjective" if pred else "Objective"
+        except Exception as e:
+            return str(e)
+    else:
+        m = Baseline(glove)
+        m.load_state_dict(torch.load(path1))
+        m.eval()
+        logit = torch.sigmoid(m(token_tensor))
+        
+        pred = torch.round(logit).long()
+        return "Subjective" if pred else "Objective"
 
 
 if __name__ == "__main__":
 
-    # m1 = Conv2dWordClassifier(glove, args1)
-    # m2 = Conv2dWordClassifier(glove, args2)
-    
     gApp = gr.Interface(fn=predict, 
                         inputs=[gr.Textbox(label="type a sentence here"), 
-                                gr.Radio(["model 1", "model 2"])], 
+                                gr.Radio(["Baseline", "CNN"])], 
                         outputs=gr.Textbox(label="Model's Prediction"))
 
     gApp.launch()
